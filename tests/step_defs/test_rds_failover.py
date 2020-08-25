5ac2ee2238713d66d2ae6e2a315a1730b167275a
dsemionov <69367140+dsemionov@users.noreply.github.com>
2020-08-25 15:59:32 -0400

    Update test_rds_failover.py

--- tests/step_defs/test_rds_failover.py
+++ tests/step_defs/test_rds_failover.py
@@ -18,7 +18,7 @@ def test_basic_duckduckgo_search():
 def the_duckduckgo_home_page_is_displayed():
     """the DuckDuckGo home page is displayed."""
     print("the DuckDuckGo home page is displayed: ")
-    pass
+    raise NotImplementedError("To be implemented")
 
 
 @when('the user searches for "panda"')
